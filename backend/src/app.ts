0c07867458965369c30dfb1bcd4a057560f15000
Natã Hermes <nhermesdev@gmail.com>
2023-04-05 13:29:57 -0300

    FEAT: add error handling
    add friendly zod-formatted error messages for error validation. I'm handling errors mapped in use cases

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify';
 import { appRoutes } from './http/routes';
+import { ZodError } from 'zod';
+import { env } from './env';
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((err, _req, reply) => {
+  if (err instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: err.format(),
+    });
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.error(err);
+  } else {
+    // TODO: Add log error to sentry/datadog/newrelic
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  });
+});
