40e487ea5ed79d5c654de0e93cbf5736837df04f
Natã Hermes <nhermesdev@gmail.com>
2023-04-05 13:25:53 -0300

    FEAT: add user controller with DIP
    I created a user creation controller with dependency injection to facilitate database maintenance and migration

---
+++ backend/src/http/controllers/register.ts
@@ -0,0 +1,37 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+import { z } from 'zod';
+import { RegisterUseCase } from '@/use-cases/register';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+
+export async function register(req: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(req.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    });
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: err.message,
+      });
+    }
+
+    throw err;
+  }
+
+  return reply.status(201).send();
+}
