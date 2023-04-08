2390f4662d05696c788a6301d0992bf32924be30
Natã Hermes <nhermesdev@gmail.com>
2023-04-08 16:14:08 -0300

    TEST: create mocks to tests

--- backend/src/use-cases/create-collaborator.spec.ts
+++ backend/src/use-cases/create-collaborator.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryCollaboratorsRepository } from '@/repositories/in-memory/in-memory-collaborator-repository';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { CreateCollaboratorUseCase } from './create-collaborator-use-case';
+import { mockCollab } from '@/utils/test/mocks/mocks-collaborators';
 
 let collaboratorsRepository: InMemoryCollaboratorsRepository;
 let sut: CreateCollaboratorUseCase;
@@ -9,18 +10,14 @@ describe('Collaborator Use Case', () => {
   beforeEach(() => {
     collaboratorsRepository = new InMemoryCollaboratorsRepository();
     sut = new CreateCollaboratorUseCase(collaboratorsRepository);
-    collaboratorMocks = {
-      name: 'John Doe',
-      type: 'producer',
-      comissionBalance: 0,
-    };
+    collaboratorMocks = mockCollab;
   });
 
   it('should be able to create collaborator', async () => {
     const { collab } = await sut.execute({
       name: collaboratorMocks.name,
       type: collaboratorMocks.type,
-      commissionBalance: collaboratorMocks.comissionBalance,
+      comissionBalance: collaboratorMocks.comissionBalance,
     });
     expect(collab.id).toEqual(expect.any(String));
   });
---
+++ backend/src/utils/test/mocks/mocks-collaborators.ts
@@ -0,0 +1,8 @@
+const mockCollab = {
+  id: 'collab-1',
+  name: 'John Doe',
+  type: 'producer',
+  comissionBalance: 0,
+};
+
+export { mockCollab };
