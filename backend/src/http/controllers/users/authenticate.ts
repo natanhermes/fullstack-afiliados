e95459058ebf6717fe923f915eab7ce300332fa8
Natã Hermes <nhermesdev@gmail.com>
2023-04-08 19:30:07 -0300

    FEAT: add refresh token to authenticate

--- backend/src/http/controllers/users/authenticate.ts
+++ backend/src/http/controllers/users/authenticate.ts
@@ -29,9 +29,27 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
       },
     );
 
-    return reply.status(200).send({
-      token,
-    });
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '1d',
+        },
+      },
+    );
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({
