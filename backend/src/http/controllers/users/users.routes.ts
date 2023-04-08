7f7f5e9bd06cd140a0d6d5f86c23791b820f3cee
Natã Hermes <nhermesdev@gmail.com>
2023-04-08 17:32:56 -0300

    FEAT: add controller and tests e2e to authentication

--- backend/src/http/controllers/users/users.routes.ts
+++ backend/src/http/controllers/users/users.routes.ts
@@ -1,7 +1,10 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
 import { FastifyInstance } from 'fastify';
 import { createUser } from './create-user';
+import { authenticate } from './authenticate';
+import { profile } from './profile';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', createUser);
+  app.post('/sessions', authenticate);
 }
