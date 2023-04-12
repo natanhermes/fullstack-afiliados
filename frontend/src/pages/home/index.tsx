88e4901fb49e1b16e968975e548e688ade79bbf6
Natã Hermes <nhermesdev@gmail.com>
2023-04-12 16:45:06 -0300

    feat: add routes to app

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import { FormLogin } from '@/components/FormLogin';
+
+export default function Home() {
+  return (
+    <>
+      <Container>
+        <FormLogin />
+      </Container>
+    </>
+  );
+}
