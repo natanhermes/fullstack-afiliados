b83d7b6f2f69546317957064273dc4db80fb0e68
Natã Hermes <nhermesdev@gmail.com>
2023-04-12 16:46:38 -0300

    feat: add enums and helpers

---
+++ frontend/src/enums/mapped-office.ts
@@ -0,0 +1,4 @@
+export enum MappedOffice {
+  producer = 'PRODUTOR',
+  affiliated = 'AFILIADO',
+}
---
+++ frontend/src/enums/mapped-transaction-type.ts
@@ -0,0 +1,6 @@
+export const mappedTransaction = {
+  '1': { description: 'Venda produtor', nature: 'Entrada', signal: '+' },
+  '2': { description: 'Venda afiliado', nature: 'Entrada', signal: '++' },
+  '3': { description: 'Comissão paga', nature: 'Saída', signal: '-' },
+  '4': { description: 'Comissão recebida', nature: 'Entrada', signal: '+' },
+};
